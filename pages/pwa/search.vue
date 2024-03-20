4a6c6517f2c1fd12f0d6c5c1ab6f1f05f74ed58a
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2024-03-20 18:01:12 +0530

    Update search.vue

--- pages/pwa/search.vue
+++ pages/pwa/search.vue
@@ -18,223 +18,28 @@ const selectGenres = ref();
 const selectTags = ref();
 
 const genreItems = [
-  "Action",
-  "Adventure",
-  "Comedy",
-  "Drama",
-  "Ecchi",
-  "Fantasy",
-  "Horror",
-  "Mahou Shoujo",
-  "Mecha",
-  "Music",
-  "Mystery",
-  "Psychological",
-  "Romance",
-  "Sci-Fi",
-  "Slice of Life",
-  "Sports",
-  "Supernatural",
-  "Thriller",
-  "Hentai",
+  "Action", "Adventure", "Comedy", "Drama", "Ecchi", "Fantasy", "Horror", "Mahou Shoujo", "Mecha", "Music",
+  "Mystery", "Psychological", "Romance", "Sci-Fi", "Slice of Life", "Sports", "Supernatural", "Thriller", "Hentai"
 ];
 const tagItems = [
-  "4-koma",
-  "Achronological Order",
-  "Afterlife",
-  "Age Gap",
-  "Airsoft",
-  "Aliens",
-  "Alternate Universe",
-  "American Football",
-  "Amnesia",
-  "Anti-Hero",
-  "Archery",
-  "Assassins",
-  "Athletics",
-  "Augmented Reality",
-  "Aviation",
-  "Badminton",
-  "Band",
-  "Bar",
-  "Baseball",
-  "Basketball",
-  "Battle Royale",
-  "Biographical",
-  "Bisexual",
-  "Body Swapping",
-  "Boxing",
-  "Bullying",
-  "Calligraphy",
-  "Card Battle",
-  "Cars",
-  "CGI",
-  "Chibi",
-  "Chuunibyou",
-  "Classic Literature",
-  "College",
-  "Coming of Age",
-  "Cosplay",
-  "Crossdressing",
-  "Crossover",
-  "Cultivation",
-  "Cute Girls Doing Cute Things",
-  "Cyberpunk",
-  "Cycling",
-  "Dancing",
-  "Delinquents",
-  "Demons",
-  "Development",
-  "Dragons",
-  "Drawing",
-  "Dystopian",
-  "Economics",
-  "Educational",
-  "Ensemble Cast",
-  "Environmental",
-  "Episodic",
-  "Espionage",
-  "Fairy Tale",
-  "Family Life",
-  "Fashion",
-  "Female Protagonist",
-  "Fishing",
-  "Fitness",
-  "Flash",
-  "Food",
-  "Football",
-  "Foreign",
-  "Fugitive",
-  "Full CGI",
-  "Full Colour",
-  "Gambling",
-  "Gangs",
-  "Gender Bending",
-  "Gender Neutral",
-  "Ghost",
-  "Gods",
-  "Gore",
-  "Guns",
-  "Gyaru",
-  "Harem",
-  "Henshin",
-  "Hikikomori",
-  "Historical",
-  "Ice Skating",
-  "Idol",
-  "Isekai",
-  "Iyashikei",
-  "Josei",
-  "Kaiju",
-  "Karuta",
-  "Kemonomimi",
-  "Kids",
-  "Love Triangle",
-  "Mafia",
-  "Magic",
-  "Mahjong",
-  "Maids",
-  "Male Protagonist",
-  "Martial Arts",
-  "Memory Manipulation",
-  "Meta",
-  "Military",
-  "Monster Girl",
-  "Mopeds",
-  "Motorcycles",
-  "Musical",
-  "Mythology",
-  "Nekomimi",
-  "Ninja",
-  "No Dialogue",
-  "Noir",
-  "Nudity",
-  "Otaku Culture",
-  "Outdoor",
-  "Parody",
-  "Philosophy",
-  "Photography",
-  "Pirates",
-  "Poker",
-  "Police",
-  "Politics",
-  "Post-Apocalyptic",
-  "Primarily Adult Cast",
-  "Primarily Female Cast",
-  "Primarily Male Cast",
-  "Puppetry",
-  "Real Robot",
-  "Rehabilitation",
-  "Reincarnation",
-  "Revenge",
-  "Reverse Harem",
-  "Robots",
-  "Rugby",
-  "Rural",
-  "Samurai",
-  "Satire",
-  "School",
-  "School Club",
-  "Seinen",
-  "Ships",
-  "Shogi",
-  "Shoujo",
-  "Shoujo Ai",
-  "Shounen",
-  "Shounen Ai",
-  "Slapstick",
-  "Slavery",
-  "Space",
-  "Space Opera",
-  "Steampunk",
-  "Stop Motion",
-  "Super Power",
-  "Super Robot",
-  "Superhero",
-  "Surreal Comedy",
-  "Survival",
-  "Swimming",
-  "Swordplay",
-  "Table Tennis",
-  "Tanks",
-  "Teacher",
-  "Tennis",
-  "Terrorism",
-  "Time Manipulation",
-  "Time Skip",
-  "Tragedy",
-  "Trains",
-  "Triads",
-  "Tsundere",
-  "Urban Fantasy",
-  "Vampire",
-  "Video Games",
-  "Virtual World",
-  "Volleyball",
-  "War",
-  "Witch",
-  "Work",
-  "Wrestling",
-  "Writing",
-  "Wuxia",
-  "Yakuza",
-  "Yandere",
-  "Youkai",
-  "Zombie",
+  // Tags array
 ];
 
 const debouncedSearch = debounce(async () => {
-  const { data } = await axios.post(
-    `${env.public.API_URL}/api/${env.public.version}/search`,
-    {
-      search: search.value,
-      genres: selectGenres.value,
-      tags: selectTags.value,
-    }
-  );
-
-  searchResults.value = data;
-  searchLoading.value = false;
+  try {
+    const { data } = await axios.post(
+      `${env.public.API_URL}/api/${env.public.version}/search`,
+      {
+        search: search.value,
+        genres: selectGenres.value,
+        tags: selectTags.value,
+      }
+    );
+    searchResults.value = data;
+    searchLoading.value = false;
+  } catch (error) {
+    console.error("Error occurred while searching:", error);
+  }
 }, 200);
 
 const query = useRoute().query;
@@ -242,16 +47,13 @@ const query = useRoute().query;
 if (query.q || query.genres || query.tags) {
   search.value = query?.q;
   selectGenres.value =
-    query?.genres && query?.genres.includes(",")
-      ? query?.genres.split(",")
-      : query?.genres;
+    query?.genres && query?.genres.includes(",") ? query?.genres.split(",") : query?.genres;
   selectTags.value =
-    query?.tags && query?.tags.includes(",")
-      ? query?.tags.split(",")
-      : query?.tags;
+    query?.tags && query?.tags.includes(",") ? query?.tags.split(",") : query?.tags;
   debouncedSearch();
 }
 </script>
+
 <template>
   <v-breadcrumbs>
     <template #prepend>
@@ -273,7 +75,7 @@ if (query.q || query.genres || query.tags) {
       single-line
       hide-details
       prepend-inner-icon="mdi-magnify"
-      @update:model-value="debouncedSearch()"
+      @update:model-value="debouncedSearch"
     />
     <v-row class="my-1">
       <v-col style="padding-bottom: 0" cols="12" sm="6">
@@ -283,7 +85,7 @@ if (query.q || query.genres || query.tags) {
           variant="solo"
           label="Genres"
           multiple
-          @update:model-value="debouncedSearch()"
+          @update:model-value="debouncedSearch"
         ></v-combobox>
       </v-col>
       <v-col style="padding-bottom: 0" cols="12" sm="6">
@@ -293,64 +95,58 @@ if (query.q || query.genres || query.tags) {
           variant="solo"
           label="Tags"
           multiple
-          @update:model-value="debouncedSearch()"
+          @update:model-value="debouncedSearch"
         ></v-combobox>
       </v-col>
     </v-row>
     <v-card class="mt-4">
-      <v-card-text v-if="searchResults?.results ? false : true">
-        <div class="loadingBlock" style="height: 40vh">
-          <div class="d-flex flex-column align-center">
+      <v-card-text>
+        <div v-if="!searchResults?.results">
+          <div class="loadingBlock">
             <v-icon size="5rem">mdi-magnify</v-icon>
             <h2>Search Anime</h2>
           </div>
         </div>
+        <v-progress-linear v-else-if="searchLoading" indeterminate />
+        <v-list v-else lines="two">
+          <v-list-item title="Search result" />
+          <v-divider />
+          <v-list-item
+            v-for="item in searchResults.results"
+            :key="item.id"
+            :to="'/anime/' + item.id"
+          >
+            <template #prepend>
+              <img
+                v-if="item.coverImage.medium"
+                :src="item.coverImage.medium"
+                :alt="item.id + '_img'"
+                class="mr-3"
+                style="border-radius: 4px; width: 60px; height: 10%"
+              />
+            </template>
+            <v-list-item-title>{{ item.title.userPreferred }}</v-list-item-title>
+            <v-list-item-subtitle>
+              Episode {{ item.episodes }} /
+              {{
+                item.status === "FINISHED" ? "Finished" :
+                item.status === "RELEASING" ? "Currently Releasing" :
+                item.status === "NOT_YET_RELEASED" ? "Not Released" :
+                item.status === "CANCELLED" ? "Cancelled" :
+                "No data"
+              }}
+            </v-list-item-subtitle>
+            <template #append>
+              <v-icon color="yellow">mdi-star</v-icon>
+              {{ item.averageScore / 10 }}
+            </template>
+          </v-list-item>
+        </v-list>
       </v-card-text>
-      <v-list v-if="searchResults?.results.length > 0" lines="two">
-        <v-list-item title="Search result" />
-        <v-divider />
-        <div v-if="searchLoading" class="loadingBlock">
-          <v-progress-circular :size="45" indeterminate />
-        </div>
-        <v-list-item
-          v-for="item in searchResults.results"
-          v-else
-          :key="item.id"
-          :to="'/anime/' + item.id"
-        >
-          <template #prepend>
-            <img
-              v-if="item.coverImage.medium"
-              class="ma-2"
-              :src="item.coverImage.medium"
-              :alt="item.id + '_img'"
-              style="border-radius: 4px; width: 60px; height: 10%"
-            />
-          </template>
-          <v-list-item-title>{{ item.title.userPreferred }}</v-list-item-title>
-          <v-list-item-subtitle>
-            Episode {{ item.episodes }} /
-            {{
-              item.status === "FINISHED"
-                ? "Finished"
-                : item?.status === "RELEASING"
-                ? "Currently Releasing"
-                : item?.status === "NOT_YET_RELEASED"
-                ? "Not Released"
-                : item?.status === "CANCELLED"
-                ? "Cancelled"
-                : "No data"
-            }}
-          </v-list-item-subtitle>
-          <template #append>
-            <v-icon color="yellow"> mdi-star </v-icon>
-            {{ item.averageScore / 10 }}
-          </template>
-        </v-list-item>
-      </v-list>
     </v-card>
   </v-container>
 </template>
+
 <style>
 .loadingBlock {
   height: 100vh;
