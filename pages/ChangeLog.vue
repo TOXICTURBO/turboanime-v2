696c8f2f0c84fa76b94698b404e7aa61d7386c35
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2024-03-21 18:19:54 +0530

    Create ChangeLog.vue

---
+++ pages/ChangeLog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Changelog :updates="changelogUpdates" />
+  </div>
+</template>
+
+<script>
+import Changelog from '@/components/Changelog.vue'; // Adjust the path as needed
+
+export default {
+  components: {
+    Changelog,
+  },
+  data() {
+    return {
+      changelogUpdates: [
+        "Update 1: Implemented new feature",
+        "Update 2: Fixed critical bug",
+        // Add more updates as needed
+      ],
+    };
+  },
+};
+</script>
