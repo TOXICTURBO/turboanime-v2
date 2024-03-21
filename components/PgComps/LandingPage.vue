a512fab5ed7ad011aeafca22b8e3a43c87c0d2af
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2024-03-21 09:10:14 +0530

    Add files via upload

--- components/PgComps/LandingPage.vue
+++ components/PgComps/LandingPage.vue
@@ -1,8 +1,52 @@
+<script setup>
+import { useStorage } from "@vueuse/core";
+const env = useRuntimeConfig();
+
+// const setHistory = ref();
+
+const history_state = useStorage("site-watch", {});
+// const get_key = useStorage("cloud-cfg", {});
+
+// if (get_key.value.enabled) {
+//   const { data } = await useFetch('/api/getData', {
+//     method: 'GET',
+//     headers: {
+//       'x-space-collection': get_key.value.deta_collection_key
+//     },
+//   })
+//   setHistory.value = data.value.data.app_user_last_data
+// } else {
+//   setHistory.value = history_state.value
+// }
+
+const {
+  data: trendingData,
+  pending: trpend,
+  refresh: trenddataRefresh,
+  error: trenderr,
+} = useFetch(
+  `${env.public.API_URL}/api/${env.public.version}/trending?limit=12`,
+  {
+    cache: "force-cache",
+  }
+);
+const {
+  data: popularData,
+  pending: popend,
+  refresh: popdataRefresh,
+  error: poperr,
+} = useFetch(
+  `${env.public.API_URL}/api/${env.public.version}/popular?limit=12`,
+  {
+    cache: "force-cache",
+  }
+);
+</script>
+
 <template>
-  <div>
-    <!-- Popular Anime Carousel -->
+  <ClientOnly>
     <v-carousel
-      class="popular-carousel d-none d-md-block"
+      class="d-none d-md-block"
       hide-delimiters
       progress="green"
       height="320px"
@@ -17,151 +61,263 @@
       >
         <div class="carousel-item">
           <img :src="item.coverImage.large" alt="Carousel Image" />
-          <div class="carousel-overlay"></div>
-          <div class="carousel-content">
+          <div class="d-flex flex-column pa-2 justify-center">
             <h2>{{ item.title.userPreferred }}</h2>
-            <p class="text--secondary">{{ item.title.native }}</p>
-            <p class="description">{{ item.description }}</p>
-            <v-btn
-              :to="'/anime/' + item.id"
-              :color="item.coverImage.color ? item.coverImage.color : 'transparent'"
-              class="read-more-btn"
-            >
-              Read more
-            </v-btn>
+            <p class="text--secondary">
+              {{ item.title.native }}
+            </p>
+            <div
+              style="
+                overflow: hidden;
+                transition: color 0.2s ease;
+                display: -webkit-box;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 2;
+              "
+              v-html="item.description"
+            />
+            <div class="pt-2">
+              <v-btn
+                :to="'/anime/' + item.id"
+                :color="
+                  item.coverImage.color ? item.coverImage.color : 'transparent'
+                "
+                append-icon="mdi-open-in-new"
+              >
+                Read more
+              </v-btn>
+            </div>
           </div>
         </div>
       </v-carousel-item>
     </v-carousel>
-
-    <!-- Trending and Popular Anime Grid -->
-    <v-container>
-      <!-- Trending Anime -->
-      <h1 class="section-title">Trending Anime</h1>
-      <v-row v-if="!trpend && !trenderr">
-        <v-col
-          v-for="(d, i) in trendingData?.results"
-          :key="i"
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
+  </ClientOnly>
+  <v-container>
+    <SearchBar />
+    <ClientOnly>
+      <div v-if="history_state?.latest_anime_watched">
+        <v-alert
+          class="mt-4"
+          icon="mdi-history"
+          title="Continue Watching : "
+          :text="`${history_state?.latest_anime_watched?.title} Episode ${
+            history_state?.latest_anime_watched?.curr_ep
+          } ${history_state?.latest_anime_watched?.isDub ? 'Dub' : 'Sub'}`"
+          closable
         >
-          <AnimeCard :data="d" />
-        </v-col>
-      </v-row>
-      <v-row v-else-if="trpend" class="loading-row">
-        <v-progress-circular :size="45" indeterminate color="primary" />
-      </v-row>
-      <v-row v-else>
-        <v-alert dense type="error" class="error-alert" title="Error" text="Error loading trending anime!" />
-        <v-btn @click="trenddataRefresh()" class="reload-btn">
+          <template #default>
+            <br />
+            <v-btn
+              class="my-2"
+              :to="
+                /\/pwa\.*/.test(useRoute().path)
+                  ? `/pwa/watch/${history_state?.latest_anime_watched?.id}-${history_state?.latest_anime_watched?.ep_id}`
+                  : `/watch/${history_state?.latest_anime_watched?.id}-${history_state?.latest_anime_watched?.ep_id}`
+              "
+              prepend-icon="mdi-play"
+            >
+              Resume?
+            </v-btn>
+          </template>
+        </v-alert>
+      </div>
+    </ClientOnly>
+  </v-container>
+  <!-- DESKTOP DEVICE -->
+  <v-container class="d-lg-block d-sm-none d-none" fluid>
+    <v-col>
+      <h1>Trending Anime</h1>
+      <div v-if="trpend" class="loadingBlock">
+        <v-progress-circular :size="45" indeterminate />
+      </div>
+      <div v-else-if="trenderr">
+        <v-alert
+          dense
+          type="error"
+          title="Error"
+          text="Error loading trending anime!"
+        />
+        <v-btn @click="trenddataRefresh()">
           Reload?
           <v-icon>mdi-reload</v-icon>
         </v-btn>
-      </v-row>
-
-      <!-- Popular Anime -->
-      <h1 class="section-title">Popular Anime</h1>
-      <v-row v-if="!popend && !poperr">
-        <v-col
-          v-for="(d, i) in popularData?.results"
-          :key="i"
-          cols="12"
-          sm="6"
-          md="4"
-          lg="3"
-        >
-          <AnimeCard :data="d" />
-        </v-col>
-      </v-row>
-      <v-row v-else-if="popend" class="loading-row">
-        <v-progress-circular :size="45" indeterminate color="primary" />
-      </v-row>
-      <v-row v-else>
-        <v-alert dense type="error" class="error-alert" title="Error" text="Error loading popular anime!" />
-        <v-btn @click="popdataRefresh()" class="reload-btn">
+      </div>
+      <v-container v-else fluid>
+        <div class="grid">
+          <div
+            v-for="(d, i) in trendingData?.results"
+            :key="i"
+            class="d-flex justify-center"
+          >
+            <AnimeCard
+              :id="d.id"
+              :title="d.title.userPreferred"
+              :imgsrc="d.coverImage.large"
+              :imgalt="d.id"
+              :anime-color="d.coverImage.color"
+              :year="d.seasonYear"
+              :type="d.format"
+              :total-ep="d.episodes"
+              :status="d.status"
+            />
+          </div>
+        </div>
+      </v-container>
+    </v-col>
+    <v-col>
+      <h1>Popular Anime</h1>
+      <div v-if="popend" class="loadingBlock">
+        <v-progress-circular :size="45" indeterminate />
+      </div>
+      <div v-else-if="poperr">
+        <v-alert
+          dense
+          type="error"
+          title="Error"
+          text="Error loading popular anime!"
+        />
+        <v-btn @click="popdataRefresh()">
           Reload?
           <v-icon>mdi-reload</v-icon>
         </v-btn>
-      </v-row>
-    </v-container>
-  </div>
+      </div>
+      <v-container v-else fluid>
+        <div class="grid">
+          <div
+            v-for="(d, i) in popularData?.results"
+            :key="i"
+            class="d-flex justify-center"
+          >
+            <AnimeCard
+              :id="d.id"
+              :title="d.title.userPreferred"
+              :imgsrc="d.coverImage.large"
+              :imgalt="d.id"
+              :anime-color="d.coverImage.color"
+              :year="d.seasonYear"
+              :type="d.format"
+              :total-ep="d.episodes"
+              :status="d.status"
+            />
+          </div>
+        </div>
+      </v-container>
+    </v-col>
+  </v-container>
+  <!-- MOBILE DEVICE -->
+  <v-container class="d-lg-none d-sm-block d-xs mb-5" fluid>
+    <h2>Trending Anime</h2>
+    <div v-if="trpend" class="loadingBlock">
+      <v-progress-circular :size="45" indeterminate />
+    </div>
+    <div v-else-if="trenderr">
+      <v-alert
+        dense
+        type="error"
+        title="Error"
+        text="Error loading trending anime!"
+      />
+      <v-btn @click="trenddataRefresh()">
+        Reload?
+        <v-icon>mdi-reload</v-icon>
+      </v-btn>
+    </div>
+    <v-row v-else>
+      <v-col class="media-scrolling">
+        <div v-for="d in trendingData?.results" :key="d.id">
+          <AnimeCard
+            :id="d.id"
+            :title="d.title.userPreferred"
+            :imgsrc="d.coverImage.large"
+            :imgalt="d.id"
+            :anime-color="d.coverImage.color"
+            :year="d.seasonYear"
+            :type="d.format"
+            :total-ep="d.episodes"
+            :status="d.status"
+          />
+        </div>
+      </v-col>
+    </v-row>
+    <h2 class="mt-10">Popular Anime</h2>
+    <div v-if="popend" class="loadingBlock">
+      <v-progress-circular :size="45" indeterminate />
+    </div>
+    <div v-else-if="poperr">
+      <v-alert
+        dense
+        type="error"
+        title="Error"
+        text="Error loading trending anime!"
+      />
+      <v-btn @click="popdataRefresh()">
+        Reload?
+        <v-icon>mdi-reload</v-icon>
+      </v-btn>
+    </div>
+    <v-row v-else>
+      <v-col class="media-scrolling">
+        <div v-for="d in popularData?.results" :key="d.id">
+          <AnimeCard
+            :id="d.id"
+            :title="d.title.userPreferred"
+            :imgsrc="d.coverImage.large"
+            :imgalt="d.id"
+            :anime-color="d.coverImage.color"
+            :year="d.seasonYear"
+            :type="d.format"
+            :total-ep="d.episodes"
+            :status="d.status"
+          />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
-<style scoped>
-.section-title {
-  font-size: 24px;
-  margin-bottom: 20px;
+<style>
+.loadingBlock {
+  height: 200px;
+  display: grid;
+  place-items: center;
 }
 
-.popular-carousel {
-  margin-bottom: 30px;
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+}
+
+.media-scrolling {
+  --_spacer: 0.6rem;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  display: grid;
+  grid-auto-flow: column;
+  overflow-x: auto;
+  overscroll-behavior-inline: contain;
+  scroll-snap-type: inline mandatory;
+  scroll-padding-inline: var(--_spacer, 1rem);
+}
+
+.media-scrolling > * {
+  scroll-snap-align: start;
+}
+
+@media (min-width: 768px) {
+  .grid {
+    grid-template-columns: repeat(auto-fit, minmax(290px, 1fr));
+  }
 }
 
 .carousel-item {
-  position: relative;
-}
-
-.carousel-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-}
-
-.carousel-content {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  color: #fff;
-  text-align: center;
-  padding: 20px;
-}
-
-.carousel-content h2 {
-  font-size: 24px;
-  margin-bottom: 10px;
-}
-
-.carousel-content .description {
-  font-size: 14px;
-  margin-bottom: 20px;
-}
-
-.read-more-btn {
-  color: #fff;
-  background-image: linear-gradient(to right, #ff8a00, #e52e71);
-  transition: background-color 0.3s;
-}
-
-.read-more-btn:hover {
-  background-image: linear-gradient(to right, #ff8a00, #ff1358);
-}
-
-.loading-row {
+  background-color: rgba(0, 0, 0, 0.7);
   display: flex;
   justify-content: center;
-  align-items: center;
-  height: 200px;
-}
-
-.error-alert {
-  background-color: #ff5252;
-  color: #fff;
-}
-
-.reload-btn {
-  margin-top: 10px;
-  color: #fff;
-  background-image: linear-gradient(to right, #00b4db, #0083b0);
-  transition: background-color 0.3s;
-}
-
-.reload-btn:hover {
-  background-image: linear-gradient(to right, #00b4db, #0083b0);
+  padding: 2.5rem;
+  height: 320px;
+  gap: 1rem;
 }
 </style>
