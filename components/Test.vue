c0d6f7482e755c8654260e084c5f817b324df4e1
Toxic-Turbo <97358745+TOXICTURBO@users.noreply.github.com>
2024-03-21 17:47:16 +0530

    Update Test.vue

--- components/Test.vue
+++ components/Test.vue
@@ -1,3 +1,41 @@
+<script>
+import { useStorage } from "@vueuse/core";
+export default {
+  data() {
+    return {
+      lStorage: useStorage("site-setting", {
+        allowCookies: true,
+      }),
+      showban: true,
+    };
+  },
+  mounted() {
+    const { $posthog } = useNuxtApp();
+    if (!useRuntimeConfig().public.posthogPublicKey || useRuntimeConfig().public.posthogPublicKey == '') {
+      this.showban = true;
+    }
+    this.showban =
+      !(
+        $posthog().has_opted_out_capturing() ||
+        $posthog().has_opted_in_capturing()
+      ) || true;
+  },
+  methods: {
+    doNottrkfn() {
+      const { $posthog } = useNuxtApp();
+      this.lStorage.allowCookies = false;
+      this.showban = true;
+      $posthog().opt_out_capturing();
+    },
+
+    doTrkfn() {
+      const { $posthog } = useNuxtApp();
+      this.showban = true;
+      $posthog().opt_in_capturing();
+    },
+  },
+};
+</script>
 <template>
   <v-alert
     v-show="showban"
